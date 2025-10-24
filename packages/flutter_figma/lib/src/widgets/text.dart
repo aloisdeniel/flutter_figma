429638e5f7dcd20dacf241a6e3ad46989d671e93
Alois Deniel <alois.deniel@gmail.com>
2025-10-24 22:55:38 +0200

    Added default text style.

--- packages/flutter_figma/lib/src/widgets/text.dart
+++ packages/flutter_figma/lib/src/widgets/text.dart
@@ -1,23 +1,82 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_figma/src/foundation/foundation.dart';
 import 'package:flutter_figma/src/rendering/text.dart';
 
-class FigmaText extends LeafRenderObjectWidget {
-  const FigmaText({
-    super.key,
+class DefaultFigmaTextStyle extends InheritedWidget {
+  const DefaultFigmaTextStyle({
+    required super.child,
     required this.style,
-    required this.characters,
-    this.fills = const [],
-    this.strokes = const [],
+    required this.fills,
+    required this.strokes,
   });
 
   final FigmaTextStyle style;
   final List<FigmaPaint> fills;
   final List<FigmaPaint> strokes;
+
+  static FigmaTextStyle fallbackStyle = FigmaTextStyle(
+    fontName: FontName(family: 'system', style: 'regular'),
+    fontSize: 12,
+    letterSpacing: LetterSpacing(
+      value: 0,
+      unit: LetterSpacingUnit.pixels,
+    ),
+    lineHeight: LineHeightAuto(),
+  );
+
+  static List<FigmaPaint> fallbackFills(BuildContext context) {
+    final brightness = MediaQuery.platformBrightnessOf(context);
+    return switch (brightness) {
+      Brightness.dark => const [
+          SolidPaint(
+            color: Color.from(alpha: 1, red: 1, green: 1, blue: 1),
+          ),
+        ],
+      Brightness.light => const [
+          SolidPaint(
+            color: Color.from(alpha: 1, red: 0, green: 0, blue: 0),
+          ),
+        ],
+    };
+  }
+
+  static DefaultFigmaTextStyle? maybeOf(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<DefaultFigmaTextStyle>();
+  }
+
+  @override
+  bool updateShouldNotify(covariant DefaultFigmaTextStyle oldWidget) {
+    return style != oldWidget.style ||
+        !listEquals(fills, oldWidget.fills) ||
+        !listEquals(strokes, oldWidget.strokes);
+  }
+}
+
+class FigmaText extends LeafRenderObjectWidget {
+  const FigmaText({
+    super.key,
+    required this.characters,
+    this.style,
+    this.fills = const [],
+    this.strokes = const [],
+  });
+
+  final FigmaTextStyle? style;
+  final List<FigmaPaint>? fills;
+  final List<FigmaPaint>? strokes;
   final List<FigmaTextSpan> characters;
 
   @override
   RenderObject createRenderObject(BuildContext context) {
+    late final defaultStyle = DefaultFigmaTextStyle.maybeOf(context);
+    final style = this.style ??
+        defaultStyle?.style ??
+        DefaultFigmaTextStyle.fallbackStyle;
+    final fills = this.fills ??
+        defaultStyle?.fills ??
+        DefaultFigmaTextStyle.fallbackFills(context);
+    final strokes = this.strokes ?? defaultStyle?.strokes ?? const [];
     return RenderFigmaText(
       style: style,
       characters: characters,
@@ -28,6 +87,14 @@ class FigmaText extends LeafRenderObjectWidget {
 
   @override
   void updateRenderObject(BuildContext context, RenderFigmaText renderObject) {
+    late final defaultStyle = DefaultFigmaTextStyle.maybeOf(context);
+    final style = this.style ??
+        defaultStyle?.style ??
+        DefaultFigmaTextStyle.fallbackStyle;
+    final fills = this.fills ??
+        defaultStyle?.fills ??
+        DefaultFigmaTextStyle.fallbackFills(context);
+    final strokes = this.strokes ?? defaultStyle?.strokes ?? const [];
     renderObject
       ..style = style
       ..characters = characters
