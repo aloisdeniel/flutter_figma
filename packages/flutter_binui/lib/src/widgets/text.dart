81ee396622e651252f62d378318c159c342bf971
Alois Deniel <alois.deniel@gmail.com>
2025-12-18 21:30:37 +0100

    Fixed text color.

--- packages/flutter_binui/lib/src/widgets/text.dart
+++ packages/flutter_binui/lib/src/widgets/text.dart
@@ -1,6 +1,7 @@
 import 'package:binui/binui.dart' as b;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_binui/src/providers.dart';
+import 'package:flutter_binui/src/value.dart';
 import 'package:flutter_figma/widgets.dart';
 
 class BinuiText extends StatelessWidget {
@@ -11,6 +12,28 @@ class BinuiText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final config = BinuiProvider.of(context);
-    return FigmaText(config.resolve(node.characters, ''));
+    return FigmaText(
+      config.resolve(node.characters, ''),
+      fills: node.fills
+          .map(
+            (p) => p.toFigmaFlutter(
+              config.library,
+              variableCollectionVariants: config.variableCollectionVariants,
+              properties: config.componentProperties,
+            ),
+          )
+          .nonNulls
+          .toList(),
+      strokes: node.strokes
+          .map(
+            (p) => p.toFigmaFlutter(
+              config.library,
+              variableCollectionVariants: config.variableCollectionVariants,
+              properties: config.componentProperties,
+            ),
+          )
+          .nonNulls
+          .toList(),
+    );
   }
 }
